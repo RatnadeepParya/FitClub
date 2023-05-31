4feebc265e1bbdf57f3dbd0bb88e84e83d5b10a0
Ratnadeep Parya <parya.ratnadeep125@gmail.com>
2023-05-31 20:30:55 +0530

    web responsive done

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Join.css";
+import emailjs from "@emailjs/browser";
 
 const Join = () => {
-    return(
-        <div className="Join" id="join-us">
-            <div className="left-j">
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_flr4kg8",
+        "template_j1w96lo",
+        form.current,
+        "VNhJWmBU7f0Su0WI1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-            </div>
-            <div className="right-j">
-
-            </div>
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
         </div>
-    )
-}
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form
+          ref={form}
+          action=""
+          className="email-container"
+          onSubmit={sendEmail}
+        >
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your Email address"
+          />
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Join
+export default Join;
